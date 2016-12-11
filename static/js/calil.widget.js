08f44e8b8e70141498e71b6a994cf8bb4b0a6367
Daichi Kikuchi <daichi.kikuchi@kikudai.com>
2016-12-12 01:09:40 +0900

    カーリルの埋め込みリンクを改修
    wordpress ->
    jekyllコンバートのときにjsがないため単なるリンクになってしまっていた。
    参考リンク https://calil.jp/widget/

---
+++ static/js/calil.widget.js
@@ -0,0 +1,1 @@
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://widget.calil.jp/v1.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','calil-wjs');
